3bdd5b297543492df848299b99917195db8bec58
Bapiya <bapiya1@gmail.com>
2025-08-09 10:03:10 +0530

    speed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./Home";
 
 function App() {
   const [hom, sethom] = useState("Welcome");
-  const [speed, setSpeed] = useState<number>(80);
+  const [speed, setSpeed] = useState<number>(1);
   const [score, setScore] = useState<number>(0);
   return (
     <div className="app">
